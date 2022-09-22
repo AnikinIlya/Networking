cefdbfc16915ea133919baa958dd1db072c01ef4
AnikinIlya <98335973+AnikinIlya@users.noreply.github.com>
2022-09-22 12:58:16 +0400

    Initial Commit

---
+++ Networking/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Networking
+//
+//  Created by Anikin Ilya on 22.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
